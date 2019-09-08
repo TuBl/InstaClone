26af4ef607ec3aad0f8f2f516da7714347f827d7
Tariq Yousef <tariqys1995@gmail.com>
2019-09-08 09:55:55 -0400

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+ <div class="row">
+     <div class="col-3 p-5">
+         <img src="https://instagram.fybz2-2.fna.fbcdn.net/vp/ceb8b7c46d41887ae2831e6a89db5440/5E038B38/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fybz2-2.fna.fbcdn.net" alt="logo" class = "rounded-circle">
+     </div>
+     <div class="col-9 pt-5">
+         <div>
+             <h1>free code camp</h1>
+         </div>
+         <div class = "d-flex">
+             <div class = "pr-5"><strong>153</strong> posts</div>
+             <div class = "pr-5"><strong>23k</strong> followers</div>
+             <div class = "pr-5"><strong>229</strong> following</div>
+         </div>
+     
+             <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+             <div >We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+             </div>
+             <div>
+                <a href="www.freecodecamp.org
+                " style = "color: #003569; font-size 16px;">www.freecodecamp.org
+               </a>
+             </div> 
+
+        </div>  
+    </div>  
+            <div class="row pt-4">
+                <div class="col-4"><img src="https://instagram.fybz2-2.fna.fbcdn.net/vp/d76fdbfd285b56f56bf1b15fd6ff701c/5E0FA738/t51.2885-15/e35/69353077_983457791986894_2219053937293359629_n.jpg?_nc_ht=instagram.fybz2-2.fna.fbcdn.net" alt="" class = "w-100 h-100"></div>
+                <div class="col-4"><img src="https://instagram.fybz2-2.fna.fbcdn.net/vp/4f26909ba9b0511a41e69c639dff19db/5DFC9844/t51.2885-15/e35/67737131_193841561646624_2063880671014995153_n.jpg?_nc_ht=instagram.fybz2-2.fna.fbcdn.net" alt="" class = "w-100 h-100"></div>
+                <div class="col-4"><img src="https://instagram.fybz2-2.fna.fbcdn.net/vp/d145dadefb0bb9c2045da94c3862428d/5E09E5A0/t51.2885-15/e35/69875934_3278854528798972_69605946931405030_n.jpg?_nc_ht=instagram.fybz2-2.fna.fbcdn.net" alt="" class = "w-100 h-100"></div>
+            </div>
+     </div>
+ </div>
+</div>
+@endsection
+
+
