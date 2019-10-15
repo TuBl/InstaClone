220e143cf1c1b8c540df15089d4b281e2f111413
Tariq <tariqys995@gmail.com>
2019-10-15 11:18:00 -0400

    desktop

--- public/js/app.js
+++ public/js/app.js
@@ -49509,8 +49509,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Coding\PHPLarvel\instagramClone\instaClone\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Coding\PHPLarvel\instagramClone\instaClone\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\User\Desktop\Tariq\WebDev\PHP\InstaClone\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\User\Desktop\Tariq\WebDev\PHP\InstaClone\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
